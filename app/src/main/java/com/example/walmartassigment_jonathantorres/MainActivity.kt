011e7f7967795dd4eec1da2255000cbecaa426ec
Johntdev15 <johntdev15@gmail.com>
2025-09-21 16:06:05 -0500

    Made a few other changes to the UI and took off un-needed elements.

--- app/src/main/java/com/example/walmartassigment_jonathantorres/MainActivity.kt
+++ app/src/main/java/com/example/walmartassigment_jonathantorres/MainActivity.kt
@@ -26,7 +26,6 @@ import com.example.walmartassigment_jonathantorres.viewmodel.CountriesViewModel
 class MainActivity : ComponentActivity() {
 
     private lateinit var recycler: RecyclerView
-    private lateinit var progress: ProgressBar
     private lateinit var errorText: TextView
     private val vm: CountriesViewModel by viewModels()
     private val adapter = CountryRVAdapter()
@@ -36,7 +35,6 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         recycler = findViewById(R.id.recycler_RV)
-        progress = findViewById(R.id.progress_PB)
         errorText = findViewById(R.id.errorText_TV)
 
         recycler.layoutManager = LinearLayoutManager(this)
@@ -45,11 +43,9 @@ class MainActivity : ComponentActivity() {
         vm.state.observe(this) { state ->
             when (state) {
                 is ViewsStates.Loading -> {
-                    progress.visibility = View.VISIBLE
                     errorText.visibility = View.GONE
                 }
                 is ViewsStates.Success -> {
-                    progress.visibility = View.GONE
                     errorText.visibility = View.GONE
                     adapter.setItems(state.data)
                     recycler.post {
@@ -60,7 +56,6 @@ class MainActivity : ComponentActivity() {
                     }
                 }
                 is ViewsStates.Error -> {
-                    progress.visibility = View.GONE
                     errorText.visibility = View.VISIBLE
                     errorText.text = state.message
                 }
